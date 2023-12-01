2c6c738f9d100de083387faffaff23fd15b37370
RafayDev <imabdulrafayy@gmail.com>
2023-11-30 21:12:51 -0800

    old

--- resources/views/pdf/query.blade.php
+++ resources/views/pdf/query.blade.php
@@ -6,130 +6,125 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quotation</title>
     <style>
-    @page {
-        margin: 0px;
-        padding: 0px;
-    }
+        @page {
+            margin: 0px;
+            padding: 0px;
+        }
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            padding-top: 100px;
+            position: relative;
+        }
 
-    body {
-        font-family: 'Arial', sans-serif;
-        margin: 0;
-        padding: 0;
-        padding-top: 100px;
-        position: relative;
-    }
+        .page-background {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-image: url('./logos/{{$company->company_logo}}');
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-size: contain;
+            z-index: -2;
+        }
 
-    .page-background {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-image: url('./logos/{{$company->company_logo}}');
-        background-repeat: no-repeat;
-        background-position: center center;
-        background-size: contain;
-        z-index: -2;
-    }
+        .opacity-layer {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: white;
+            opacity: 0.85;
+            pointer-events: none;
+            z-index: -1;
+        }
 
-    .opacity-layer {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: white;
-        opacity: 0.85;
-        pointer-events: none;
-        z-index: -1;
-    }
+        .header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 80px;
+            background-color: #ffffff;
+            border-bottom: 1px solid #eee;
+            padding: 10px;
+            z-index: 1000;
+        }
 
-    .header {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 80px;
-        background-color: #ffffff;
-        border-bottom: 1px solid #eee;
-        padding: 10px;
-        z-index: 1000;
-    }
+        .header img {
+            width: 140px;
+            margin-left: 20px; 
+        }
 
-    .logo img {
-        width: 140px;
-        margin-left: 20px;
-        padding-top: 120px;
-        padding-bottom: 130px;
-    }
+        .container {
+            width: 95%;
+            margin: 2em auto;
+        }
 
-    .container {
-        width: 90%;
-        margin: 2em auto;
-    }
+        h1, h2 {
+            margin: 0.5em 0;
+        }
 
-    h1,
-    h2 {
-        margin: 0.5em 0;
-    }
+        h1 {
+            text-align: center;
+            font-size: 24px;
+        }
 
-    h1 {
-        text-align: center;
-        font-size: 24px;
-    }
+        h2 {
+            text-align: center;
+            font-size: 20px;
+            color: #666;
+        }
 
-    h2 {
-        text-align: center;
-        font-size: 20px;
-        color: #666;
-    }
+        h3 {
+            text-align: right;
+            font-size: 16px;
+            color: #999;
+        }
 
-    h3 {
-        text-align: right;
-        font-size: 16px;
-        color: #999;
-    }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 2em;
+        }
 
-    table {
-        width: 100%;
-        border-collapse: collapse;
-        margin-top: 2em;
-    }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 0.5em 1em;
+        }
 
-    th,
-    td {
-        border: 1px solid #ddd;
-        padding: 0.5em 1em;
-    }
+        th {
+            background-color: #f5f5f5;
+        }
 
-    th {
-        background-color: #f5f5f5;
-    }
+        td {
+            text-align: center;
+        }
 
-    td {
-        text-align: center;
-    }
+        .footer {
+            position: fixed;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            background-color: #2c3e50;
+            padding: 20px 0;
+            z-index: 999;
+        }
 
-    .footer {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background-color: #2c3e50;
-        padding: 10px 0;
-        z-index: 999;
-    }
+        .footer-content {
+            width: 100%;
+            margin: 0 auto;
+            color: #ecf0f1;
+        }
 
-    .footer-content {
-        width: 100%;
-        margin: 0 auto;
-        color: #ecf0f1;
-    }
-
-    .footer-content p {
-        text-align: center;
-        margin: 10px 0;
-    }
+        .footer-content p {
+            text-align: center;
+            margin: 10px 0;
+        }
     </style>
 </head>
 
@@ -138,11 +133,9 @@
     <div class="opacity-layer"></div>
 
     <div class="header">
-        <div class="logo">
-            <img src="./logos/{{$company->company_logo}}" alt="Company Logo">
-        </div>
+        <img src="./logos/{{$company->company_logo}}" alt="Company Logo">
     </div>
-
+    
     <div class="container">
         <h1>Quotation</h1>
         <h2>{{$company->company_name}}</h2>
@@ -171,8 +164,6 @@
                 @endforeach
             </tbody>
         </table>
-        <div style="text-align:right; margin-top:100px;">>
-        </div>
     </div>
 
     <div class="footer">
