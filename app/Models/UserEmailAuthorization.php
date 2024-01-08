49247cd6f0149c5b38c75bf2ee2662b53105ffc8
RafayDev <imabdulrafayy@gmail.com>
2024-01-08 21:24:47 +0500

    fixes

--- app/Models/UserEmailAuthorization.php
+++ app/Models/UserEmailAuthorization.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class UserEmailAuthorization extends Model
 {
     use HasFactory;
+    protected $table = 'user_email_authorizations';
+    protected $primaryKey = 'id';
 }
