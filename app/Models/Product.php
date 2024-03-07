072226ea10fa378464eba7574e23f1627c9af36b
RafayDev <imabdulrafayy@gmail.com>
2024-03-07 19:53:14 +0500

    query update

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,8 @@ class Product extends Model
     {
         return $this->hasMany(ProductSize::class, 'product_id', 'id');
     }
+    function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
